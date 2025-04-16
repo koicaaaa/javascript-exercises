63aff84a88711fc0f9df3c95f114964cf56af021
Leonna <koicaaaaaa@gmail.com>
2025-04-16 17:59:00 -0400

    Java exercises to review on

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    for (i = 0; i < num; i++) {
+        word = word.repeat(i);
+    }
 };
 
 // Do not edit below this line
